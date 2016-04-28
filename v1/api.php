e4141cc6a4bc7e9763f13f9674368a1a7ce8d7b4
Jaime Caballero <lamatesoft@gmail.com>
2016-04-28 23:49:33 +0100

    Test routing

--- v1/api.php
+++ v1/api.php
@@ -4,3 +4,9 @@
 $app->get('/', function($req, $res){
     return $res->withStatus(301)->withHeader('Location', 'https://fontperf.com');
 });
+
+$app->get('/v1/gfonts/{fontstring}', function($req, $res){
+	echo json_encode([ 'fontstring' => $req->getAttribute()['fontstring'] ]);
+
+    return $res;
+});

0be07a751e182b60c5115de2229c14bf618826f0
Travis CI <me@jaicab.com>
2016-04-28 21:44:17 +0100

    Correct syntax on deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,7 +2,7 @@
 
 set -o errexit -o nounset
 
-if [ $TRAVIS_PULL_REQUEST == "true" || "$TRAVIS_BRANCH" != "master" ]
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]
 then
   echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
   exit 0
@@ -12,7 +12,7 @@ rev=$(git rev-parse --short HEAD)
 
 git init
 git config user.name "Travis CI"
-git config user.email "me@jaicab.com"
+git config user.email "nobody@fontperf.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/jaicab/api.fontperf.com.git"
 git fetch upstream

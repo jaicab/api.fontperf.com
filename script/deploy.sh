48a4b8af837b4f14496f76024dc29fc819dad2f3
Travis CI <nobody@fontperf.com>
2016-04-28 21:47:47 +0000

    Launch 0a9348d to production

---
+++ script/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Travis CI"
+git config user.email "nobody@fontperf.com"
+
+git remote add upstream "https://${GITHUB_TOKEN}@github.com/jaicab/api.fontperf.com.git"
+git fetch upstream
+git reset upstream/production
+
+
+cat > .gitignore << LINES
+./.travis.yml
+./.gitignore
+./README.md
+./LICENSE
+./phpunit.xml
+./composer.json
+./composer.lock
+./script/deploy.sh
+LINES
+
+touch .
+
+git add -A .
+git commit -m "Launch ${rev} to production"
+git push -q upstream HEAD:production

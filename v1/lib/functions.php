7d358e1a4e567a03ea2569151f985bc76f607281
Jaime Caballero <lamatesoft@gmail.com>
2016-05-06 13:23:21 +0100

    Single file download seems to work

--- v1/lib/functions.php
+++ v1/lib/functions.php
@@ -1,5 +1,6 @@
 <?php
 use \Curl\Curl;
+use Sabberworm\CSS\Parser as CSSParser;
 
 
 
@@ -10,6 +11,11 @@ function processURL($oURL) {
 
 	return $ret;
 }
+
+function encodeSpacing($str) {
+	return str_replace(" ", "+", $str);
+}
+
 
 function breakDownFamily($family) {
 	$ret = [];
@@ -24,10 +30,10 @@ function breakDownFamily($family) {
 
 		if(sizeof($name_style)>1) {
 			foreach($styles as $style) {
-				$ret[] = urlencode($name.':'.$style);
+				$ret[] = encodeSpacing($name.':'.$style);
 			}
 		} else {
-			$ret[] = urlencode($name);
+			$ret[] = encodeSpacing($name);
 		}
 	}
 
@@ -35,34 +41,36 @@ function breakDownFamily($family) {
 }
 
 function fetchCombinedCss($family, $userAgentList = []) {
+	$userAgentList = [
+		"woff2" => "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/38.0.2125.104 Safari/537.36",
+		"woff" => "Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko",
+		"ttf" => "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/534.54.16 (KHTML, like Gecko) Version/5.1.4 Safari/534.54.16"
+	];
+
 	$ret = [
 		'error' => null,
 		'data' => null
 	];
 
+	// Every format but EOT
 	foreach($userAgentList as $type => $ua) {
+		$fetchedCSS = fetchCSS($family, $ua);
+		if(!is_null($fetchedCSS['error'])) {
+			return $fetchCSS;
+		} 
+		$ret['data'] .= $fetchedCSS['data'];
+	}
 
-		if($type == "eot") {
+	// EOT needs one request per style and weight
+	$ua = "Mozilla/5.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; GTB7.4; InfoPath.2; SV1; .NET CLR 3.3.69573; WOW64; en-US)";
+	$family_breakdown = breakDownFamily($family);
 
-			$family_breakdown = breakDownFamily($family);
-
-			foreach($family_breakdown as $font) {
-				$fetchedCSS = fetchCSS($font, $ua);
-				if(!is_null($fetchedCSS['error'])) {
-					return $fetchCSS;
-				} 
-				$ret['data'] .= $fetchedCSS['data'];
-			}
-
-		} else {
-
-			$fetchedCSS = fetchCSS($family, $ua);
-			if(!is_null($fetchedCSS['error'])) {
-				return $fetchCSS;
-			} 
-			$ret['data'] .= $fetchedCSS['data'];
-
-		}
+	foreach($family_breakdown as $font) {
+		$fetchedCSS = fetchCSS($font, $ua);
+		if(!is_null($fetchedCSS['error'])) {
+			return $fetchCSS;
+		} 
+		$ret['data'] .= $fetchedCSS['data'];
 	}
 
 
@@ -101,4 +109,96 @@ function fetchCSS($family, $userAgent = '', $addComment = false) {
 	$curl->close();
 
 	return $ret;
+}
+
+
+function processCSS($stringCSS) {
+	$font_list = [];
+
+	$oParser = new CSSParser($stringCSS);
+
+	$oCss = $oParser->parse();
+	$oCssContents = $oCss->getContents();
+
+	foreach($oCssContents as $font) {
+		$fontRuleList = $font->getRules();
+		$name = null;
+		$weight = 400;
+		$style = 'normal';
+		$src = null;
+		
+		// Get name
+		foreach($fontRuleList as $fontRule) {
+			if($fontRule->getRule() == "font-family") {
+				$name = trim(strval($fontRule->getValue()), '"');
+			}
+			if($fontRule->getRule() == "font-weight") {
+				$weight = intval(strval($fontRule->getValue()));
+			}
+			if($fontRule->getRule() == "font-style") {
+				$style = strval($fontRule->getValue());
+			}
+			if($fontRule->getRule() == "src") {
+				$src = $fontRule->getValue();
+			}
+		}
+
+		$styleID = encodeSpacing($name. ':' . $weight . ($style!="normal" ? $style : ''));
+
+		// Ignore if no name
+		if(is_null($name) || is_null($src))
+			break;
+
+		// Add family to font list
+		if(!isset($font_list[$name])) {
+			$font_list[$name] = [];
+			$font_list[$name]['family'] = $name;
+			$font_list[$name]['types'] = [];
+		}
+
+		// Add font variation
+		if(!isset($font_list[$name]['types'][$styleID])) {
+			$font_list[$name]['types'][$styleID] = new stdClass();
+		}
+		$font_list[$name]['types'][$styleID]->id = $styleID;
+		$font_list[$name]['types'][$styleID]->weight = $weight;
+		$font_list[$name]['types'][$styleID]->style = $style;
+
+
+		// Files
+		if(!isset($font_list[$name]['types'][$styleID]->files)) {
+			$font_list[$name]['types'][$styleID]->files = new stdClass();
+		}
+		// Break down src
+		if($src instanceof Sabberworm\CSS\Value\URL) {
+			$details = processURL($src);
+			$font_list[$name]['types'][$styleID]->files->$details['ext'] = $details['file'];
+		
+		} elseif($src instanceof Sabberworm\CSS\Value\RuleValueList) {
+		
+			$components = $src->getListComponents();
+			if($components[0] instanceof Sabberworm\CSS\Value\RuleValueList) {
+				$components = $components[0]->getListComponents();
+			}
+
+			foreach($components as $bit) {
+				if($bit instanceof Sabberworm\CSS\Value\URL) {
+					$details = processURL($bit);
+					$font_list[$name]['types'][$styleID]->files->$details['ext'] = $details['file'];
+					break;
+				}
+			}
+		} else {
+			var_dump($src);
+			die();
+		}
+	}
+
+	// Make list of styles an unordered array
+	foreach($font_list as $key => $family) {
+		$font_list[$key]['types'] = array_values($family['types']);
+		$font_list[$key] = (object) $font_list[$key];
+	}
+
+	return array_values($font_list);
 }

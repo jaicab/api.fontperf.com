9d13ccbaa19be70c269c1a0911253b8f65024459
Jaime Caballero <lamatesoft@gmail.com>
2016-05-07 16:44:17 +0100

    Add documentation to GFont class

--- v1/lib/GFont.php
+++ v1/lib/GFont.php
@@ -3,7 +3,14 @@
 use \Curl\Curl;
 use Sabberworm\CSS\Parser as CSSParser;
 
-
+/**
+ * GFont - Google Fonts service handler
+ *
+ * This class handles requests to Google Fonts and transforms
+ * the response into useful setups or font downloads
+ * @author Jaime Caballero <me@jaicab.com>
+ * @version 1.0
+ */
 class GFont {
 
   const BASEURL = 'https://fonts.googleapis.com/css?family=';
@@ -17,6 +24,15 @@ class GFont {
   public $errorMessage = null;
   public $errorStatus = null;
 
+  public $count = 0;
+
+
+  /**
+   * Constructor sets the initial data and fetches the CSS
+   *
+   * @access public
+   * @param  string $family Value of the query string with the same name provided by Google Fonts
+   */
   function __construct($family) {
     $this->family = urlencode($family);
     $this->font_list = [];
@@ -24,6 +40,14 @@ class GFont {
     $this->fetchCombinedCSS();
   }
 
+
+  /**
+   * Sets an error to be detected on instance level
+   *
+   * @access private
+   * @param $message Message to be shown on error
+   * @param $status Status code to return if any
+   */
   private function setError($message, $status = null) {
     $this->error = true;
     $this->errorMessage = $message;
@@ -32,6 +56,13 @@ class GFont {
   }
 
 
+  /**
+   * Breaks down family into styles for separate requests
+   *
+   * @access private
+   * @param $message Message to be shown on error
+   * @param $status Status code to return if any
+   */
   private function breakDownFamily() {
     $ret = [];
 
@@ -53,16 +84,21 @@ class GFont {
     }
 
     $this->family_breakdown = $ret;
-
-    return;
+    $this->count = count($ret);
   }
 
-
-
-  private function fetchCSS($userAgent = '', $addComment = false) {
+  /**
+   * Fetches the CSS for a given family string
+   *
+   * @access private
+   * @param $family Family string from GF
+   * @param $userAgent User agent string for different output
+   * @return string CSS contents as a string
+   */
+  private function fetchCSS($family, $userAgent = '') {
     $ret = '';
 
-    $url = strval(self::BASEURL.$this->family);
+    $url = strval(self::BASEURL.$family);
     $curl = new Curl();
     $curl->setUserAgent($userAgent);
     $curl->get($url);
@@ -78,6 +114,12 @@ class GFont {
     return $ret;
   }
 
+
+  /**
+   * Fetches all the necessary CSS to get all the formats we want
+   *
+   * @access private
+   */
   private function fetchCombinedCSS() {
     $ret = '';
 
@@ -89,7 +131,7 @@ class GFont {
 
     // Every format but EOT
     foreach($userAgentList as $type => $ua) {
-      $fetchedCSS = $this->fetchCSS($ua);
+      $fetchedCSS = $this->fetchCSS($this->family, $ua);
       $ret .= $fetchedCSS;
     }
 
@@ -98,14 +140,18 @@ class GFont {
     $this->breakDownFamily();
 
     foreach($this->family_breakdown as $font) {
-      $fetchedCSS = $this->fetchCSS($ua);
+      $fetchedCSS = $this->fetchCSS($font, $ua);
       $ret .= $fetchedCSS;
     }
 
     $this->css = $ret;
-    return;
   }
 
+  /**
+   * Builds an clean object that contains all the families, styles, and files in different formats.
+   *
+   * @access public
+   */
   public function buildList() {
     $font_list = [];
     $oParser = new CSSParser($this->css);
@@ -197,7 +243,12 @@ class GFont {
     return;
   }
 
-
+  /**
+   * Takes the contents of the font list and builds a CSS using the given pattern
+   *
+   * @access public
+   * @param string $type 'normal' for external font requests, 'datauri' for woff2 data-URIs.
+   */
   public function buildCSS($type = 'normal') {
 
     $oCss = new Sabberworm\CSS\CSSList\Document();
@@ -275,10 +326,24 @@ class GFont {
     return $oCss->render($oFormat);
   }
 
+
+  /**
+   * Lists the font files, styles and families
+   *
+   * @access public
+   * @return object Font list breakdown
+   */
   public function getList() {
     return $this->font_list;
   }
 
+
+  /**
+   * Combined CSS for all the different formats
+   *
+   * @access public
+   * @return string Contents of all the requested CSS in a string
+   */
   public function getCss() {
     return $this->css;
   }

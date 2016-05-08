1a2875e2552074d57ed4afbd4350e58f4cc11541
Travis CI <nobody@fontperf.com>
2016-05-08 11:39:11 +0000

    Launch a3f66be to production

--- v1/lib/functions.php
+++ v1/lib/functions.php
@@ -4,8 +4,21 @@ use \Curl\Curl;
 function processURL($oURL) {
 	$ret = [];
 	$ret['file'] = trim(strval($oURL->getURL()), '"');
-	$ext = pathinfo($ret['file'], PATHINFO_EXTENSION);
-  $ret['ext'] = empty($ext) ? "woff2" : $ext;
+
+  $curl = new Curl();
+  $curl->get($ret['file']);
+
+  $mime = $curl->responseHeaders['Content-Type'];
+  if( ($slash_pos = strpos($mime, '/')) !== FALSE )
+   $ret['ext'] = substr($mime, $slash_pos + 1);
+
+  $valid_extensions = ['woff2', 'woff', 'ttf', 'eot'];
+
+  if(!in_array($ret['ext'], $valid_extensions)) {
+    $ret['ext'] = 'unknown';
+  }
+
+  $curl->close();
 
 	return $ret;
 }
@@ -15,3 +28,28 @@ function encodeSpacing($str) {
 }
 
 // TODO CSS comment function, each line an item from array
+function cssComment($disclaimer = false, $lines = [], $version = '1.0') {
+  $ret = "/*!\n * fontperf API v".$version." (https://fontperf.com/docs/)\n * Fonts provided by Google Fonts - https://www.google.com/fonts\n";
+
+  if(!empty($lines)) {
+    array_unshift($lines, " ");
+  }
+
+  if($disclaimer) {
+    $lines[] = " ";
+
+    if(is_string($disclaimer)) $lines[] = $disclaimer;
+    else {
+      $lines[] = "DISCLAIMER: DO NOT LINK THIS CSS DIRECTLY.";
+      $lines[] = "THIS API IS NOT MEANT FOR HOSTING AND ANY UPDATE COULD AFFECT YOUR CODEBASE.";
+    }
+  }
+
+  foreach($lines as $line) {
+    $ret .= " * ". $line ."\n";
+  }
+
+  $ret .= "*/\n";
+
+  return $ret;
+}
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit2c1af81d6aec58c9ebe7c8453d155bf9::getLoader();
+return ComposerAutoloaderInita8e60dcecc5f5d7af7b29bf43b3a7c09::getLoader();
